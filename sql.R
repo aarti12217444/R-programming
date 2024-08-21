222acb1cd65726bf4a9b09707ff9d8da1f3934cb
Aarti Kumari <119749450+aarti12217444@users.noreply.github.com>
2024-08-21 10:28:15 +0530

    Add files via upload

---
+++ sql.R
@@ -0,0 +1,17 @@
+#commands
+
+
+#comparsion data filter cmd
+#showed command to view data(order by col name desc,ase)
+#display first n rows use limit fun.
+#right operator=match a pattern(%)
+#aggrrigate fun()=count,avf,min,max,summery
+#group by(madea grp of similar type of thing)
+#in fun()(match only one value)
+library(shiny)
+
+library(leaflet)
+library(plotly)
+library(dplyr)
+
+
