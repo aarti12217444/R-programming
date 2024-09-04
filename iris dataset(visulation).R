468c704e779f03728f4e5148a44365b75dfdb9ec
Aarti Kumari <119749450+aarti12217444@users.noreply.github.com>
2024-09-04 11:03:12 +0530

    Update iris dataset(visulation).R

--- iris dataset(visulation).R
+++ iris dataset(visulation).R
@@ -11,11 +11,11 @@ normalize<-function(x){
 a1=as.data.frame(lapply(a[2:3L],normalize))
 summary(a$Sepal.Length)
 a_train=a1[1:120,]
-a_teat=a1[122:150,]
+a_test=a1[122:150,]
 a_train_lables=a1[1:120,1]
-a_teat_lables=a1[121:150,1]
+a_test_lables=a1[121:150,1]
 a_train_lables
-a_teat_lables
+a_test_lables
 
 if (!require(class)) install.packages("class")
 library(class)
